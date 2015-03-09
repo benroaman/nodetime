9e2c0b9f300a8d08ee73524724de9f2bb9f50cff
Ben Roaman <benroaman@gmail.com>
2015-03-09 11:40:27 -0400

    added dummy data, got body-parser.json working

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var express = require('express'), // include the dependency express
+    bodyParser = require('body-parser'),
     app = express();
+    friends = [{
+      name: 'James',
+      sex: 'm'
+    }, {
+      name: 'Garrett',
+      sex: 'm'
+    }, {
+      name: 'Meredith',
+      sex: 'f'
+    }];
+
+app.use(bodyParser.json());
 
 app.get('/api/friends', function (req, res) { // req is the request object, sent by data, res is how you respond to req
-  res.send('Hello, world!');
+  res.json(friends);
 });
 
 // Start the server
