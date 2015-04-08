a754ef128e255c744d6c5b0ef257792c4099aadd
Ben Roaman <benroaman@gmail.com>
2015-04-08 09:27:35 -0400

    pushing to github

--- app.js
+++ app.js
@@ -1,29 +1,67 @@
 var express = require('express'), // include the dependency express
     bodyParser = require('body-parser'),
     app = express();
-    friends = [{
-      name: 'James',
-      sex: 'm'
-    }, {
-      name: 'Garrett',
-      sex: 'm'
-    }, {
-      name: 'Meredith',
-      sex: 'f'
-    }];
+    Nedb = require('nedb'),
+    db = new Nedb();
 
 app.use(bodyParser.json());
-
 app.use(express.static(__dirname + '/src'));
 app.use('/dist', express.static(__dirname + '/dist'));
 
 app.get('/api/friends', function (req, res) { // req is the request object, sent by data, res is how you respond to req
-  res.json(friends);
+  db.find({}).sort({ sortName: 1 }).exec(function (err, friends) {
+   res.json(friends);
+ });
 });
 
+app.get('/api/friends/names', function (req, res) {
+  db.find({}, { lastName: 1, firstName: 1 }, function (err, names) { // using projections to limit returned fields
+   res.json(names);
+ });
+});
+
+app.get('/api/friends/:id', function (req, res) { // find a single friend by id
+  db.find({ _id: req.param('id') }, function (err, friend) {
+   res.json(friend);
+ });
+});
+
+app.put('/api/friends/:id', function (req, res) {
+  var friendUpdate = req.body;
+  friendUpdate.sortName = friendUpdate.lastName.toLowerCase() + friendUpdate.firstName.toLowerCase();
+  db.update({ _id: req.param('id') }, friendUpdate, {}, function(err, numReplaced, newDoc) { // replace friend with updated version
+    res.json(newDoc);
+  });
+});
+
+app.put('/api/friends/:id/favorite', function (req, res) {
+  db.update({ _id: req.param('id') }, { $set: { isFavorite: req.body.isFavorite } }, {}, function(err, numReplaced, newDoc) {
+    res.json(newDoc);
+  });
+});
+
+app.delete('/api/friends/:id', function (req, res) {
+  db.remove({ _id: req.param('id') }, {});
+})
+
 app.post('/api/friends', function (req, res) {
-  friends.push(req.body);
-  res.json(req.body);
+
+  var friend = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    sortName: req.body.lastName.toLowerCase() + req.body.firstName.toLowerCase(),
+    hometown: req.body.hometown,
+    occupation: req.body.occupation,
+    email: req.body.email,
+    phone: req.body.phone,
+    isFavorite: false,
+    sex: req.body.sex
+  };
+
+  db.insert(friend, function (err, friendRecord) {
+    res.json(friendRecord);
+  });
+
 })
 
 // Start the server
--- src/js/main.js
+++ src/js/main.js
@@ -5,14 +5,97 @@ refreshList();
 function refreshList() {
   $.getJSON('/api/friends').done(function (data) {
     $('.friends-list').html(listTemplate({ friends: data }));
+    $('.main-edit-button').on('click', activateEdit);
+    $('.edit-button').click(function(e) {
+      var self = $(e.target);
+      self.toggle();
+      self.nextAll('.edit-input').toggle();
+    });
+    $('.favorite-button').on('click', toggleFavorite);
+    $('.delete-button').on('click', function(e) {
+      var id = $(e.target).closest('li').attr('data');
+      $.ajax({
+        type: "DELETE",
+        url: '/api/friends/' + id,
+      }).done(refreshList());
+    })
   });
+};
+
+function toggleFavorite(e) {
+  self = $(e.target);
+  var id = $(e.target).closest('li').attr('data');
+
+  if (self.hasClass('favorite')) {
+    $.ajax({
+      type: "PUT",
+      url: '/api/friends/' + id + '/favorite',
+      data: JSON.stringify({ isFavorite: false }),
+      contentType : 'application/json',
+      dataType: 'json'
+    }).done(refreshList());
+  } else {
+    $.ajax({
+      type: "PUT",
+      url: '/api/friends/' + id + '/favorite',
+      data: JSON.stringify({ isFavorite: true }),
+      contentType : 'application/json',
+      dataType: 'json'
+    }).done(refreshList());
+  }
 }
 
-$('.new-friend-form').submit(function(e) {
+function activateEdit(e) {
+  var self = $(e.target);
+  self.closest('li').find('.edit-button').toggle();
+  self.nextAll('.listing-button').toggle();
+  self.text('y');
+  self.off('click', activateEdit);
+  self.on('click', submitEdit);
+}
+
+function submitEdit(e) {
+  var self = $(e.target).closest('li');
+  var id = self.attr('data');
+  var friend = {
+    _id: id,
+    firstName: self.find('.first-name-edit').val() || self.find('.first-name-display').text(),
+    lastName: self.find('.last-name-edit').val() || self.find('.last-name-display').text(),
+    hometown: self.find('.hometown-edit').val() || self.find('.hometown-display').text(),
+    occupation: self.find('.occupation-edit').val() || self.find('.occupation-display').text(),
+    email: self.find('.email-edit').val() || self.find('.email-display').text(),
+    phone: self.find('.phone-edit').val() || self.find('.phone-display').text(),
+    isFavorite: self.find('.favorite-button').hasClass('favorite'),
+    sex: self.find('h2').data()
+  }
+
+  $.ajax({
+    type: "PUT",
+    url: '/api/friends/' + id,
+    data: JSON.stringify(friend),
+    contentType : 'application/json',
+    dataType: 'json'
+  }).done(refreshList());
+}
+
+$('.new-friend-form').on('submit', addFriend);
+
+
+function addFriend(e) {
   e.preventDefault();
 
+  if (!validateInput()) {
+    alert('all fields required');
+    return;
+  }
+
   var newFriend = {
-    name: $('.friend-name').val(),
+    firstName : $('.friend-first-name').val(),
+    lastName : $('.friend-last-name').val(),
+    hometown : $('.friend-hometown').val(),
+    occupation : $('.friend-occupation').val(),
+    email : $('.friend-email').val(),
+    phone : $('.friend-phone').val(),
     sex: $('input[name=sex]:checked').val()
   };
 
@@ -22,6 +105,30 @@ $('.new-friend-form').submit(function(e) {
     data: JSON.stringify(newFriend),
     contentType : 'application/json',
     dataType: 'json'
-  }).done(refreshList);
+  }).done(refreshList());
 
-});
+  $('.friend-input').val('');
+  $('input[name=sex]').attr('checked', false);
+
+}
+
+function validateInput() {
+  var result = true;
+  $('.friend-input').each(function (i) {
+    if ($(this).val() === '') {
+      result = false;
+      return;
+    }
+  });
+
+  // if (!$('input[name=sex]:checked').length) {
+  //   result = false;
+  // }
+
+  return result;
+}
+
+$('.main-edit-button').click(function(e) {
+  var self = $(e.target);
+  self.closest('li').find('.edit-button').toggleClass('visible');
+})
